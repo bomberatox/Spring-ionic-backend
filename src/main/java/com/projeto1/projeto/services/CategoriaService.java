13ed07e669879ff9771d9609cb21d0d1b0935499
bomberatox <bomberatox@gmail.com>
2020-06-25 10:04:10 -0300

    Validacao Bean Validation

--- src/main/java/com/projeto1/projeto/services/CategoriaService.java
+++ src/main/java/com/projeto1/projeto/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.projeto1.projeto.domain.Categoria;
+import com.projeto1.projeto.dto.CategoriaDTO;
 import com.projeto1.projeto.repositories.CategoriaRepository;
 import com.projeto1.projeto.services.exceptions.DataIntegrityException;
 import com.projeto1.projeto.services.exceptions.ObjectNotFoundException;
@@ -56,4 +57,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
 }

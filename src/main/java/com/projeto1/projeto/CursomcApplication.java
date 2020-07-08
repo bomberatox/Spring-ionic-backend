a6da6d2ce8e545f6e8da863e987e1f1d77218834
bomberatox <bomberatox@gmail.com>
2020-07-08 13:49:15 -0300

    busca de produtos por nome e categorias

--- src/main/java/com/projeto1/projeto/CursomcApplication.java
+++ src/main/java/com/projeto1/projeto/CursomcApplication.java
@@ -71,6 +71,14 @@ public class CursomcApplication implements CommandLineRunner {
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
+		Produto p4 = new Produto(null, "Mesa de escritório", 250.00);
+		Produto p5 = new Produto(null, "Teclado", 120.00);
+		Produto p6 = new Produto(null, "Tela", 350.00);
+		Produto p7 = new Produto(null, "Gabinete gamer", 300.00);
+		Produto p8 = new Produto(null, "Estabilizador", 90.00);
+		Produto p9 = new Produto(null, "Cabo fonte", 20.00);
+		Produto p10 = new Produto(null, "Cabo HDMI", 40.00);
+		Produto p11 = new Produto(null, "Mesa digitalizadora", 500.00);
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
@@ -108,12 +116,25 @@ public class CursomcApplication implements CommandLineRunner {
 		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
 		cli1.getTelefones().addAll(Arrays.asList("991082466", "99710069"));
 		
-		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
-		cat2.getProdutos().addAll(Arrays.asList(p2));
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3,p6));
+		cat2.getProdutos().addAll(Arrays.asList(p2,p8,p4));
+		cat3.getProdutos().addAll(Arrays.asList(p7));
+		cat4.getProdutos().addAll(Arrays.asList(p3));
+		cat5.getProdutos().addAll(Arrays.asList(p5));
+		cat6.getProdutos().addAll(Arrays.asList(p7));
+		cat7.getProdutos().addAll(Arrays.asList(p11,p10,p9));
 		
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat1));
+		p3.getCategorias().addAll(Arrays.asList(cat1,cat4));
+		p4.getCategorias().addAll(Arrays.asList(cat2));
+		p5.getCategorias().addAll(Arrays.asList(cat5));
+		p6.getCategorias().addAll(Arrays.asList(cat1));
+		p7.getCategorias().addAll(Arrays.asList(cat3,cat6));
+		p8.getCategorias().addAll(Arrays.asList(cat2));
+		p9.getCategorias().addAll(Arrays.asList(cat7));
+		p10.getCategorias().addAll(Arrays.asList(cat7));
+		p11.getCategorias().addAll(Arrays.asList(cat7));
 		
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est1.getCidades().addAll(Arrays.asList(c2,c3));
@@ -121,7 +142,7 @@ public class CursomcApplication implements CommandLineRunner {
 		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
-		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11));
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		clienteRepository.saveAll(Arrays.asList(cli1));
--- src/main/java/com/projeto1/projeto/resources/exception/FieldMessage.java
+++ src/main/java/com/projeto1/projeto/resources/exception/FieldMessage.java
@@ -4,16 +4,18 @@ import java.io.Serializable;
 
 public class FieldMessage implements Serializable {
 	private static final long serialVersionUID = 1L;
-
+	
 	private String fieldName;
 	private String message;
 	
+	public FieldMessage() {
+	}
+
 	public FieldMessage(String fieldName, String message) {
 		super();
 		this.fieldName = fieldName;
 		this.message = message;
 	}
-
 
 	public String getFieldName() {
 		return fieldName;
@@ -30,5 +32,4 @@ public class FieldMessage implements Serializable {
 	public void setMessage(String message) {
 		this.message = message;
 	}
-	
 }

0cbde751e9d82bb6b3559a411032ae1b8c05aabf
bomberatox <bomberatox@gmail.com>
2020-07-16 11:13:31 -0300

    Email HTML

--- src/main/java/com/projeto1/projeto/services/EmailService.java
+++ src/main/java/com/projeto1/projeto/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.projeto1.projeto.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.projeto1.projeto.domain.Pedido;
@@ -9,4 +11,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
